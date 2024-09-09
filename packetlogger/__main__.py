9381e3cddd4de3781936e1e0413a339770fe0020
cvasqxz <cvxz@pm.me>
2024-09-09 16:27:37 -0300

    arreglo de codigo bonito

--- packetlogger/__main__.py
+++ packetlogger/__main__.py
@@ -1,46 +1,19 @@
-from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from socket import socket, AF_INET, SOCK_STREAM
-from json import loads, dumps, JSONDecodeError
 from threading import Thread
 from queue import Queue
-from RC4 import RC4
 
 
-class webserver(BaseHTTPRequestHandler):
-	def log_message(self, format, *args):
-		pass
-
-	def do_POST(self):
-		length = self.headers.get('content-length')
-		request_data = self.rfile.read(int(length))
-
-		try:
-			json_request = loads(request_data)
-			print("webserver request: ", json_request)
-			self.send_response(200)
-
-		except JSONDecodeError as e:
-			print("webserver error:", e)
-			self.send_response(500)
+G = 23786635532332886537261431906453031264918297
+P = 632158881801130885249042417232212770524741295422564233061391190031954228421232913648184592218883487397503624904102572293826728806813079
 
 
-def server_process(server_socket, server_queue, cypher):
-	decrypt = False
-
+def server_process(server_socket, server_queue):
 	while True:
 		server_data = server_socket.recv(1024)
 		if len(server_data) == 0:
 			exit()
 
-		if decrypt:
-			decrypted_packet = cypher.crypt(server_data)
-			print("SEND:", str(decrypted_packet)[2:-1])
-		else:
-			print("SEND:", str(server_data)[2:-1])
-
-		if server_data == b'@@BCJ':
-			decrypt = True
-
+		print("SEND:", str(server_data)[2:-1])
 		server_queue.put(server_data)
 
 
@@ -55,21 +28,24 @@ def client_process(client_socket, client_queue):
 
 
 def main_process(server_socket, client_socket, server_queue, client_queue):
-	while True:
-		while not server_queue.empty():
-			server_packet = server_queue.get()
-			client_socket.sendall(server_packet)
+	try:
+		while True:
+			while not server_queue.empty():
+				server_packet = server_queue.get()
+				client_socket.sendall(server_packet)
 
-		while not client_queue.empty():
-			client_packet = client_queue.get()
-			server_socket.sendall(client_packet)
+			while not client_queue.empty():
+				client_packet = client_queue.get()
+				server_socket.sendall(client_packet)
+
+	except:
+		print("Socket error, quitting...")
+		exit()
 
 
 def main(address, port):
 	server_queue = Queue()
 	client_queue = Queue()
-
-	decryptoor = RC4(0, "776169776f72696e616f776169776f72696e616f")
 
 	s = socket(AF_INET, SOCK_STREAM)
 	s.bind(('localhost', port))
@@ -82,7 +58,7 @@ def main(address, port):
 
 	client_socket = socket(AF_INET, SOCK_STREAM)
 	client_socket.connect((address, port))
-	print("Packetlogger connected to Habbo server")
+	print("Packetlogger connected to Habbo Server")
 
 	client_thrad = Thread(
 		target=client_process,
@@ -91,7 +67,7 @@ def main(address, port):
 
 	server_thread = Thread(
 		target=server_process, 
-		args=[server_socket, server_queue, decryptoor]
+		args=[server_socket, server_queue]
 		)
 
 	main_thread = Thread(
@@ -105,9 +81,6 @@ def main(address, port):
 
 	print("MITM attack successful")
 
-	webserver_thread = ThreadingHTTPServer(("localhost", 8080), webserver)
-	webserver_thread.serve_forever()
-
 	server_thread.join()
 	client_thrad.join()
 	main_thread.join()
@@ -115,9 +88,11 @@ def main(address, port):
 
 if __name__ == '__main__':
 	ADDRESS = '18.199.57.67'
-	PORT = 40001
+	PORT = 40_001
 
 	try:
 		main(ADDRESS, PORT)
+
 	except KeyboardInterrupt:
 		print("\nbye")
+		exit()
