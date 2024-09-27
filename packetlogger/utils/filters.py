dc04eae91127f837734a5baf093d9172030157af
cvasqxz <cvxz@pm.me>
2024-09-27 16:36:13 -0300

    añadido encoding latino

--- packetlogger/utils/filters.py
+++ packetlogger/utils/filters.py
@@ -1,12 +1,14 @@
 import string, re
 
 
-PRINTABLE = string.ascii_letters + string.punctuation + string.digits + chr(32)
+PRINTABLE = string.ascii_letters + \
+            string.punctuation + \
+            string.digits + \
+            'áéíóúñÁÉÍÓÚÑ' + chr(32)
 
 
 def bin2str(binary_packet):
 	string_packet = binary_packet
-
 	non_ascii = [c for c in binary_packet if chr(c) not in PRINTABLE]
 
 	for non_ascii_char in set(non_ascii):
@@ -14,12 +16,11 @@ def bin2str(binary_packet):
 		str_char = f"[{non_ascii_char}]".encode()
 		string_packet = string_packet.replace(bin_char, str_char) 
 
-	return string_packet.decode("utf-8")
+	return string_packet.decode("latin-1")
 
 
 def str2bin(string_packet):
-	binary_packet = string_packet.encode("utf-8")
-
+	binary_packet = string_packet.encode("latin-1")
 	regex_find = re.findall(r"\[(\d+)\]", string_packet)
 	
 	for replaced_char in set(regex_find):
